5e492e4053ed0109f6a43d9020937c1693bb0ef8
luoyujian <601513795@qq.com>
2023-06-25 11:54:25 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,19 +2,19 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '84e978909f3c447d8350c18dee513e7f',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '个人博客',
+  domain: 'https://radical-venom-9d7.notion.site/RUST-84e978909f3c447d8350c18dee513e7f?pvs=4',
+  author: 'LuoYuJian',
 
   // open graph metadata (optional)
-  description: 'Example Next.js Notion Starter Kit Site',
+  description: '个人博客',
 
   // social usernames (optional)
   twitter: 'transitive_bs',
